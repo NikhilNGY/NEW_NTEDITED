dc17979762e3aecaf5b6971c9e7ca36807e55a62
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-15 22:08:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/TheSilentTeam'>The Silent Team</a>\n○ Backup Channel : @TheSilentTeam\n○ Support Group : @TheSilentTeam</b>",
+            text = f"<b>╭────[ ᴍʏ ᴅᴇᴛᴀɪʟs ]────⍟\n├⍟ Mʏ Nᴀᴍᴇ : Nᴏᴛʜɪɴɢ \n├⍟ Dᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/Nikhil5757h'>Oᴡɴᴇʀ</a>\n├⍟ Cʜᴀɴɴᴇʟ : <a href='https://t.me/KR_PICTURE'>Cʟɪᴄᴋ Hᴇʀᴇ</a>\n├⍟ ɢʀᴏᴜᴘ : <a href='https://t.me/Interworld_groups'>Cʟɪᴄᴋ Hᴇʀᴇ</a>\n├⍟ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ: <a href='https://t.me/Nikhil5757h'>Cᴏɴᴛᴀᴄᴛ Oᴡɴᴇʀ</a>\n├⍟ Bᴜɪʟᴅ Sᴛᴀᴛᴜs : V1.0 [ ꜱᴛᴀʙʟᴇ ]\n╰───────────────⍟</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
