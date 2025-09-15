1195889217b21926a31f5554a1bd7048aee989c8
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-15 22:27:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -15,7 +15,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Cʟᴏꜱᴇ ", callback_data = "close")
+                        InlineKeyboardButton("● 🔒 Cʟᴏꜱᴇ ● ", callback_data = "close")
                     ]
                 ]
             )
